8bfffec37b36de0797da92520947010c7657c416
maciej <mbarkowski10@gmail.com>
2018-12-01 22:24:33 +0100

    after reverting amended commit

--- flank_prot.py
+++ flank_prot.py
@@ -6,83 +6,94 @@
 # NAZWA: flank_prot.py
 
 import json
+from yaml import load
+from collections import OrderedDict
 
 class BreakFunc(Exception): pass
 
 class FlankProt:
 	
-	def __init__(self, filename, orientation):
-		with open(filename, "r") as f:
-			elements_file = json.load(f)
+	def __init__(self, filename, ort):
+		with open(filename, "r") as f: # for YAML
+			elements_file = load(f)
 		self.elements = elements_file["routes"]
-		self.orientation = orientation
+		self.ort = ort
+		self.route = OrderedDict()
+		self.route_list = list()
 	
 	def search_overlap(self, neighbour):
 		'''szuka drogi ochronnej (nast. el. za sem. koncowym drogi przebiegu)'''
 		print(f"{self.elements[neighbour]['name']} w drodze ochronnej")
-				
+	
+	def set_pos(self, pos):
+		if pos == "N":
+			return "P"
+		return "N"
+	
+	def check_flank(self, item, op_pos, nxt):
+		# czy element jest semaforem skierowanym w dobrą stronę - może dawać ochr. boczną
+		if self.elements[item]["type"] == "signal" and nxt == "sas1":
+			return 1
+		# czy element jest zwr skierowaną w dobrą stronę - może dawać ochr. boczną
+		elif self.elements[item]["type"] == "switch" and \
+			self.elements[item]["orientation"] == op_pos:
+			return 1
+		# czy element jest końcem toru - nie może dawać ochrony bocznej - kończy wyszukiwanie ochr. bocznej
+		elif self.elements[item]["type"] == "eot":
+			return 2
+
 	def search_flank_prot(self, start_item, start_next):
 		pos = self.elements[start_item]["orientation"]
 		nr = self.elements[start_item]["nr"]
-		next = start_next
-		item = start_item
-		if pos == "N":
-			op_pos = "P"
-		else:
-			op_pos = "N"
-		points = {}
-		last_point = {}
+		nxt, item = start_next, start_item
+		points, last_point = {}, {}
+		op_pos = self.set_pos(pos)		
 		try:
 			while True:
-				neighb_item = f"element{self.elements[item][next]}"
-				# czy element jest semaforem skierowanym w dobrą stronę - może dawać ochr. boczną
-				if self.elements[item]["type"] == "signal" and \
-					next == "sas1":
-					print(f"sem {self.elements[item]['name']} w ochr. bocz.", end="")
-					nr, item, points = self.flank_end(start_item, points, last_point)
+				neighb_item = f"element{self.elements[item][nxt]}"
+				if self.check_flank(item, op_pos, nxt) == 1:	
+					self.route[self.elements[item]['name']] = "ob"
+					item = self.flank_end(start_item, points, last_point)
+					neighb_item = f"element{self.elements[item][start_next]}"			
+				elif self.check_flank(item, op_pos, nxt) == 2:
+					item = self.flank_end(start_item, points, last_point)
 					neighb_item = f"element{self.elements[item][start_next]}"
-				# czy element jest zwr skierowaną w dobrą stronę - może dawać ochr. boczną
-				elif self.elements[item]["type"] == "switch" and \
-					self.elements[item]["orientation"] == op_pos:
-					print(f"zwr {self.elements[item]['name']} w ochr. bocz.", end="")
-					nr, item, points = self.flank_end(start_item, points, last_point)
-					neighb_item = f"element{self.elements[item][start_next]}"
-				# czy element jest końcem toru
-				elif self.elements[item]["type"] == "eot":
-					nr, item, points = self.flank_end(start_item, points, last_point)
-					neighb_item = f"element{self.elements[item][start_next]}"
-				#else:
-				#	print(f"{item}, Nie znaleziono elementu ochrony bocznej")
-				
+				else:
+					pass # w tym przypadku to nie jest el. dający ochr. boczną
+				# zwr kierunkowa w ścieżce szukania ochr. bocznej
 				if self.elements[item]["type"] == "switch" and \
 					self.elements[item]["orientation"] == pos and \
 					item != start_item:
 					last_point = self.elements[item]["name"]
+
+					nxt = self.check_point(points, last_point)
+					neighb_item = f"element{self.elements[item][nxt]}"
 					
-					if last_point not in points:
-						points[last_point] = "sas2"
-					next = points[last_point]
-					neighb_item = f"element{self.elements[item][next]}"
-					
-				next, item, nr = self.next_element(neighb_item, nr)
-					
+				nxt, nr, item = self.next_element(neighb_item, nr)
 		except BreakFunc:
-			pass
-			# print("End of searching flank prot")
+			pass # koniec szukania ochrony bocznej
+	
+	def check_point(self, points, last_point):
+		'''sprawdza czy zwrotnica jest w liście'''
+		if last_point not in points:
+			points[last_point] = "sas2" # kier. szuk. drogi przeb.
+		return points[last_point]
 	
 	def next_element(self, neighb, nr):
 		'''przejście do kolejnego elementu'''
-		next = self.check_element_dir(neighb, nr)
+		nxt = self.check_element_dir(neighb, nr)
 		nr = self.elements[neighb]["nr"]
 		item = f"element{nr}"
-		return next, item, nr
+		return nxt, nr, item
 	
 	def flank_end(self, item, points, last_point):
+		'''sprawdza ost. zwrotnice w ochronie bocznej, ustawia sąsiedni element dla
+		nowej scieżki szukania drogi ochronnej'''
 		points = self.check_last_point(points, last_point)
 		if not points:
 			raise BreakFunc
 		nr = self.elements[item]["nr"]
-		return nr, f"element{nr}", points
+		return f"element{nr}"
 
 	def check_element_dir(self, neighbour, nr):
 		'''sprawdza czy sąsiedni element nie wskazuje na poprzedni, zwraca sąsiada'''
@@ -92,6 +103,7 @@ class FlankProt:
 		return "sas2"
 		
 	def check_last_point(self, switches, last_switch):
+		'''sprawdza ost. zwrotnicę na liście'''
 		if any(switches):
 			if switches[last_switch] == "sas2":
 				switches[last_switch] = "sas3"
--- routes_class.py
+++ routes_class.py
@@ -1,108 +1,99 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# klasa bazowa routes
+# klasa bazowa routes - zasadniczy moduł wyszukiwania dróg przebiegu
 # WYKONAWCA: MB
 # UTWORZONY: 03-07-2018
 # NAZWA: routes_class.py
 
 import json
+import pickle
+from collections import OrderedDict
+
 from flank_prot import FlankProt
 
 # TODO: 
-# drogi ochronne za sem. - jako dodatkowy parametr dla sem. 
-# że należy zamknąć w drodze ochronnej kolejny element,
-# ochrona boczna - wyszukiwanie ochrony bocznej przez zwrotnicę,
-# dorobić osobny moduł do tego - importowany
-
-# wyszukiwanie ochrony bocznej na zwr powinno być niezależnie od tego
-# czy jest przejezdzana z ostrza czy na ostrze
+# 1. elementy typu string taie jak: signal, eot, dj pobierać z pliku config
+# 2. ochrona boczna - wyszukiwanie ochrony bocznej przez zwrotnicę,
+# osobny moduł - in progress, uwaga - wyszukiwanie ochrony bocznej dla zwr powinno
+# być realizowane niezależnie od tego czy jest przejezdzana z ostrza czy na ostrze
+# 3. argparse - help
 
 class BreakFunc(Exception): pass
 
 class Routes(FlankProt):
 	
-	def __init__(self, filename, orientation):
-		super().__init__(filename, orientation)
+	def __init__(self, fn, ort):
+		super().__init__(fn, ort)
 
-	def search_elements(self, element, orientation):
+	def check_route_end(self, neighb_item, item, name):
+		'''sprawdza jakiego typu jest element kończący przebieg'''
+		# czy element jest końcem drogi przebiegu (semaforem), 
+		if self.elements[item]["type"] == "signal" and \
+			self.elements[item]["name"] != name and \
+			self.elements[item]["orientation"] == self.ort:
+			if self.elements[item]["prot"]: # szukaj urządzeń w drodze ochr.
+				self.search_overlap(neighb_item)
+			return True
+		# czy element jest końcem drogi przebiegu (line interface)
+		elif self.elements[item]["type"] == "li":
+			return True
+		# czy element jest końcem drogi przebiegu (koniec toru)
+		elif self.elements[item]["type"] == "eot":
+			return True
+		return False
+		
+	def search_elements(self, element, ort):
 		'''zasadnicza funkcja do przeszukiwania jsona'''
 		nr = self.elements[element]["nr"]
 		name = self.elements[element]["name"]
 		item = f"element{nr}" # etykieta elementu początkowego
-		next = "sas2"
+		nxt = "sas2"
 		points = {}
 		last_point = ""
-		print("\nPrzebieg: ", end="")
-		
-		try:
-			while True:	
-				print(f"{self.elements[item]['name']}, ", end="")
-				neighb_item = f"element{self.elements[item][next]}" # etykieta elementu sąsiedniego
-				
-				# czy element jest końcem drogi przebiegu (np. semaforem), 
-				# całego ifa zamknąć w funkcje?
-				if self.elements[item]["type"] == "signal" and \
-					self.elements[item]["name"] != name and \
-					self.elements[item]["orientation"] == self.orientation:
 
-					if self.elements[item]["prot"]: # szukaj urządzeń w drodze ochr.
-						self.search_overlap(neighb_item)
-					
-					nr, next, item = self.route_end(element, points, last_point)
-					neighb_item = f"element{self.elements[item][next]}"
+		try:		
+			while True:
+				self.route[self.elements[item]['name']] = "dj"
+				neighb_item = f"element{self.elements[item][nxt]}" # etykieta elementu sas
+				if self.check_route_end(neighb_item, item, name):
+					nr = self.route_end(element, points, last_point)
+					item = f"element{nr}"
+					neighb_item = f"element{self.elements[item]['sas2']}"
 
-				# czy element jest końcem drogi przebiegu (line iface)
-				elif self.elements[item]["type"] == "li":
-					nr, next, item = self.route_end(element, points, last_point)
-					neighb_item = f"element{self.elements[item][next]}"
-				
-				# czy element jest końcem drogi przebiegu (koniec toru)
-				elif self.elements[item]["type"] == "eot":
-					nr, next, item = self.route_end(element, points, last_point)
-					neighb_item = f"element{self.elements[item][next]}"
-
-				if self.elements[item]["type"] == "switch": # and \
-					if self.elements[item]["orientation"] == self.orientation:
+				if self.elements[item]["type"] == "switch":
+					if self.elements[item]["orientation"] == self.ort:
 						last_point = self.elements[item]["name"] # ost. zwr. w przebiegu
-					
-						if last_point not in points: # jeżeli ost. zwr. nie ma w points
-							points[last_point] = "sas2"
-					
-						next = points[last_point] # kier. szuk. drogi przeb.
-						neighb_item = f"element{self.elements[item][next]}"
-						flank = self.flank_dir(next) # kier. szuk. ochr. bocz.
-					# self.search_flank_prot(item, flank)
-				
-					# elif self.elements[item]["type"] == "switch" and \
-					# elif self.elements[item]["orientation"] != self.orientation:
+						
+						nxt = self.check_point(points, last_point)
+						neighb_item = f"element{self.elements[item][nxt]}"
+						
+						flank = self.flank_dir(nxt) # kier. szuk. ochr. bocz.
+					# elif self.elements[item]["orientation"] != self.ort:
+					#	flank = self.check_point_dir(neighb_item, nr)
 					self.search_flank_prot(item, flank)
 				
 				flank = self.check_point_dir(neighb_item, nr) # kier. szuk. ochr. bocz.
-				next, item, nr = self.next_element(neighb_item, nr) # kolejny el.
+				nxt, nr, item = self.next_element(neighb_item, nr) # kolejny el.
 
 		except BreakFunc: # jeżeli wyjątek to opuść funkcje i pętle
-			print(f"\nKoniec przebiegów rozpoczynających się od sygnału: {self.elements[element]['name']}")
-	
-	# def flank_dir(self, next):
-		# if next == "sas2":
-			# return "sas3"
-		# elif next == "sas3":
-			# return "sas2"
-	
-	def flank_dir(self, next):
-		if next == "sas3":
+			print(f"\nKoniec przebiegów od sygnału: {self.elements[element]['name']}")
+			
+	def flank_dir(self, nxt):
+		if nxt == "sas3":
 			return "sas2"
 		return "sas3"
 	
 	def route_end(self, element, points, last_point):
-		'''sprawdza ostatnią zwrotnicę w przebiegu, ustawia sąsiedni element dla nowego przebiegu'''
+		'''sprawdza ost. zwrotnicę w przebiegu, ustawia sąsiedni element
+		dla nowego przebiegu'''
 		points = self.check_last_point(points, last_point)
+		self.write_route()
 		if not points:
 			raise BreakFunc # jeżeli słownik zwrotnic jest pusty to wyjątek
 		nr = self.elements[element]["nr"]
 		item = f"element{nr}"
-		print(f"\nPrzebieg: {self.elements[item]['name']}, ", end="")
-		return nr, "sas2", f"element{nr}"
+		self.route[self.elements[item]['name']] = "dj" # ustaw pierwszy el. nowej drogi przebiegu
+		return nr
 	
 	def check_point_dir(self, neighbour, nr):
 		'''sprawdza kierunek szukania ochr bocznej kiedy jazda z ostrza'''
@@ -110,10 +101,17 @@ class Routes(FlankProt):
 			return "sas3"
 		elif self.elements[neighbour]["sas3"] == nr:
 			return "sas2"
+		return None
 
+	def write_route(self):
+		self.route_list.append(self.route)
+		with open("dict.pkl", "wb") as p:
+			pickle.dump(self.route_list, p)
+		self.route = OrderedDict()
+			
 if __name__== "__main__":
-	filename = "routes2.json"
-	r = Routes(filename, "P") # kierunek wyszukiwania przebiegów - P lub N
+	fn = "routes2.json"
+	r = Routes(fn, "P") # kierunek wyszukiwania przebiegów - P lub N
 
 	
 	
